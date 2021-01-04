aa791d35aed10040682707f761717e0a8abe18e2
aoustry@gmail.com <aoustry@gmail.com>
2021-01-04 09:04:14 +0100

    Line command call

--- Application1_reformulation.py
+++ Application1_reformulation.py
@@ -10,158 +10,159 @@ import sys
 import numpy as np
 import time
 
-# # Sample input data
-# n = 5
-# #asym = np.array([[3.7,-0.2,0,1,3],[2,2,1,0,0],[0,0,3,1,2],[1,0,0.4,5,2],[1.2,0,0.4,-1,3]])
-# asym = 5*np.random.rand(n,n)
-# Qref = 0.5*(asym + asym.transpose())
-# #qref = np.array([-2.3,-2,1,2,1])
-# qref = 3*np.random.rand(n)
-# #cref = 2
-# cref= 2*np.random.rand()
-# sigma = 0.3
-# p = 5000
-# wlist = np.random.rand(p,n)
-# noise = np.random.normal(loc = 0, scale = sigma, size = p)
-# noiseless_z=0.5 *np.array([ w.dot(Qref).dot(w) for w in wlist]) + wlist.dot(qref) + cref
-# print(noiseless_z.min())
-# z = noiseless_z + noise
-# wlist_square_flattened = np.array([(w.reshape(n,1).dot(w.reshape(1,n))).reshape(n**2) for w in wlist]) 
-# #for each vector w, we create a matrix n x n (given by w^T * w) and then a vector n**2
-
-# #Definition of the LL polytope : [0,1]^n box
-# A = np.concatenate([np.eye(n),-np.eye(n)])
-# b = np.concatenate([np.ones(n),np.zeros(n)])
-# rho = np.sqrt(n)
-
-# #Write the .dat file
-# moment=time.strftime("%Y-%b-%d__%H_%M_%S",time.localtime()) #to have different .dat files for each python run
-# f = open("./instance"+moment+".dat","w")
-# f.write("param n := %d;\n"%n)
-# f.write("param p_max := %d;\n"%p)
-# f.write("param r_dim := 0;\n")
-# f.write("\nparam Q_ref :")
-# for i in range(n):
-#     f.write(" %d "%(i+1))
-# f.write(":=\n")
-# for i in range(n):
-#     for j in range(n):
-#         if j==0:
-#             f.write("   %d "%(i+1))
-#         f.write("%f "%Qref[i,j])
-#         if j==(n-1):
-#             if i==(n-1):
-#                 f.write(";")
-#             f.write("\n")
-
-# f.write("\nparam q_ref := ")
-# for i in range(n):
-#     f.write("%d "%(i+1))
-#     f.write("%f "%qref[i])
-# f.write(";\n\nparam c_ref := %f ;\n\n"%cref)
-# f.write("\nparam w:")
-# for i in range(n):
-#     f.write(" %d "%(i+1))
-# f.write(":=\n")
-# for i in range(p):
-#     for j in range(n):
-#         if j==0:
-#             f.write("   %d "%(i+1))
-#         f.write("%f "%wlist[i,j])
-#         if j==(n-1):
-#             if i==(p-1):
-#                 f.write(";")
-#             f.write("\n")
-# f.write("\nparam epsilon := ")
-# for i in range(p):
-#     f.write("%d "%(i+1))
-#     f.write("%f "%noise[i])
-# f.write(";\n")
-# f.write("\nparam z := ")
-# for i in range(p):
-#     f.write("%d "%(i+1))
-#     f.write("%f "%z[i])
-# f.write(";")
-# f.close()
-
-name = "test"
-#Loading data
-Qref = np.load("Application1_data/"+name+"/bigQref.npy")
-qref = np.load("Application1_data/"+name+"/qref.npy")
-n = len(qref)
-cref = float(np.load("Application1_data/"+name+"/cref.npy"))
-wlist = np.load("Application1_data/"+name+"/w.npy")
-noise = np.load("Application1_data/"+name+"/noise.npy")
-z = np.load("Application1_data/"+name+"/z.npy")
-noiseless_z=0.5 *np.array([ w.dot(Qref).dot(w) for w in wlist]) + wlist.dot(qref) + cref
-print(np.linalg.norm(z-noiseless_z-noise))
-
-wlist_square_flattened = np.array([(w.reshape(n,1).dot(w.reshape(1,n))).reshape(n**2) for w in wlist]) 
-# #for each vector w, we create a matrix n x n (given by w^T * w) and then a vector n**2
-
-#Definition of the LL polytope : [0,1]^n box
-A = np.concatenate([np.eye(n),-np.eye(n)])
-b = np.concatenate([np.ones(n),np.zeros(n)])
-rho = np.sqrt(n)
-
-with Model("App1") as M:
+def __main__(name):
+    #Loading data
+    Qref = np.load("Application1_data/"+name+"/bigQref.npy")
+    qref = np.load("Application1_data/"+name+"/qref.npy")
+    n = len(qref)
+    cref = float(np.load("Application1_data/"+name+"/cref.npy"))
+    wlist = np.load("Application1_data/"+name+"/w.npy")
+    noise = np.load("Application1_data/"+name+"/noise.npy")
+    z = np.load("Application1_data/"+name+"/z.npy")
+    noiseless_z=0.5 *np.array([ w.dot(Qref).dot(w) for w in wlist]) + wlist.dot(qref) + cref
+    print(np.linalg.norm(z-noiseless_z-noise))
     
-    #Upper level var
-    obj = M.variable("t", 1, Domain.unbounded())
-    Q = M.variable("Q", [n,n], Domain.unbounded())
-    q = M.variable("q", n, Domain.unbounded())
-    c = M.variable("c", Domain.unbounded())
+    wlist_square_flattened = np.array([(w.reshape(n,1).dot(w.reshape(1,n))).reshape(n**2) for w in wlist]) 
+    # #for each vector w, we create a matrix n x n (given by w^T * w) and then a vector n**2
     
-    #LL variables
-    lam = M.variable("lambda", len(b), Domain.greaterThan(0.0))
-    alpha = M.variable("alpha", Domain.greaterThan(0.0))
-    beta = M.variable("beta", Domain.unbounded())
+    #Definition of the LL polytope : [0,1]^n box
+    A = np.concatenate([np.eye(n),-np.eye(n)])
+    b = np.concatenate([np.ones(n),np.zeros(n)])
+    rho = np.sqrt(n)
     
-    #Vars for PSD constraint
-
-    PSDVar = M.variable(Domain.inPSDCone(n+1)) #the whole matrix that must be PSD in (27)
-    PSDVar_main = PSDVar.slice([0,0], [n,n]) #we take the first submatrix n x n in each component of the sum in (27) i.e. 0.5*Q, 0_n, alpha*I_n
-    PSDVar_vec = Var.flatten(PSDVar.slice([0,n], [n,n+1])) #we take the second submatrix n x 1 i.e. 0.5*q, \sum_r(lambda_r*A)
-    PSDVar_offset = PSDVar.slice([n,n], [n+1,n+1])   #we take the third submatrix 1 x 1 i.e. \beta, alpha*1
+    with Model("App1") as M:
+        
+        #Upper level var
+        obj = M.variable("t", 1, Domain.unbounded())
+        Q = M.variable("Q", [n,n], Domain.unbounded())
+        q = M.variable("q", n, Domain.unbounded())
+        c = M.variable("c", Domain.unbounded())
+        
+        #LL variables
+        lam = M.variable("lambda", len(b), Domain.greaterThan(0.0))
+        alpha = M.variable("alpha", Domain.greaterThan(0.0))
+        beta = M.variable("beta", Domain.unbounded())
+        
+        #Vars for PSD constraint
     
-    #Objective
-    deg0term = Expr.mul(c,np.ones(p))
-    deg1term = Expr.mul(wlist, q)
-    deg2term = Expr.mul(0.5,Expr.mul(wlist_square_flattened, Var.flatten(Q)))
+        PSDVar = M.variable(Domain.inPSDCone(n+1)) #the whole matrix that must be PSD in (27)
+        PSDVar_main = PSDVar.slice([0,0], [n,n]) #we take the first submatrix n x n in each component of the sum in (27) i.e. 0.5*Q, 0_n, alpha*I_n
+        PSDVar_vec = Var.flatten(PSDVar.slice([0,n], [n,n+1])) #we take the second submatrix n x 1 i.e. 0.5*q, \sum_r(lambda_r*A)
+        PSDVar_offset = PSDVar.slice([n,n], [n+1,n+1])   #we take the third submatrix 1 x 1 i.e. \beta, alpha*1
+        
+        #Objective
+        deg0term = Expr.mul(c,np.ones(p))
+        deg1term = Expr.mul(wlist, q)
+        deg2term = Expr.mul(0.5,Expr.mul(wlist_square_flattened, Var.flatten(Q)))
+        
+        prediction_term =  Expr.add(deg2term,Expr.add(deg1term,deg0term)) #predicted z (p points)
+        M.constraint( Expr.vstack(obj, Expr.sub(prediction_term, z)), Domain.inQCone() ) #see below -- vstack put obj "above" (z-zpredicted) 
+        M.objective( ObjectiveSense.Minimize, obj )
+        #we minimize obj s.t. obj**2 >= \sum_p (z_p-zpredicted_p)**2 -> It is like we are minimizing the square root of LSE 
+        
+        #Symmetry constraint for Q
+        M.constraint( Expr.sub(Q, Q.transpose()),  Domain.equalsTo(0,n,n) ) #Q-Q^T = 0
+       
+        # c - (\lambda^T b + \alpha (1+ rho^2) + beta) >=0 
+        LL_obj_expr = Expr.add(Expr.dot(lam,b),Expr.add(Expr.mul((1+rho**2),alpha),beta))
+        M.constraint(Expr.sub(c,LL_obj_expr),Domain.greaterThan(0.0))
+        
+        #Constraints to define the several parts of the PSD matrix
+        M.constraint(Expr.sub(Expr.add(Expr.mul(0.5,Q), Expr.mul(alpha,np.eye(n))), PSDVar_main),  Domain.equalsTo(0,n,n) )  
+        M.constraint( Expr.sub(Expr.add(Expr.mul(0.5,q), Expr.mul(lam,A)), PSDVar_vec),  Domain.equalsTo(0,n) )
+        M.constraint( Expr.sub(Expr.add(beta, alpha), PSDVar_offset),  Domain.equalsTo(0) )
     
-    prediction_term =  Expr.add(deg2term,Expr.add(deg1term,deg0term)) #predicted z (p points)
-    M.constraint( Expr.vstack(obj, Expr.sub(prediction_term, z)), Domain.inQCone() ) #see below -- vstack put obj "above" (z-zpredicted) 
-    M.objective( ObjectiveSense.Minimize, obj )
-    #we minimize obj s.t. obj**2 >= \sum_p (z_p-zpredicted_p)**2 -> It is like we are minimizing the square root of LSE 
     
-    #Symmetry constraint for Q
-    M.constraint( Expr.sub(Q, Q.transpose()),  Domain.equalsTo(0,n,n) ) #Q-Q^T = 0
-   
-    # c - (\lambda^T b + \alpha (1+ rho^2) + beta) >=0 
-    LL_obj_expr = Expr.add(Expr.dot(lam,b),Expr.add(Expr.mul((1+rho**2),alpha),beta))
-    M.constraint(Expr.sub(c,LL_obj_expr),Domain.greaterThan(0.0))
+        # Solve
+        M.setLogHandler(sys.stdout)            # Add logging
+        M.writeTask("App1.ptf")                # Save problem in readable format
+        M.solve()
     
-    #Constraints to define the several parts of the PSD matrix
-    M.constraint(Expr.sub(Expr.add(Expr.mul(0.5,Q), Expr.mul(alpha,np.eye(n))), PSDVar_main),  Domain.equalsTo(0,n,n) )  
-    M.constraint( Expr.sub(Expr.add(Expr.mul(0.5,q), Expr.mul(lam,A)), PSDVar_vec),  Domain.equalsTo(0,n) )
-    M.constraint( Expr.sub(Expr.add(beta, alpha), PSDVar_offset),  Domain.equalsTo(0) )
-
-
-    # Solve
-    M.setLogHandler(sys.stdout)            # Add logging
-    M.writeTask("App1.ptf")                # Save problem in readable format
-    M.solve()
-
-    #Get results
-    print("Objective value ={0}".format(obj.level()**2))
-    print("Matrix Q")
-    print(Q.level().reshape(n,n))
-    print("Matrix Q_ref")
-    print(Qref)
-    print("Vector q")
-    print(q.level())
-    print("Vector q_ref")
-    print(qref)
-    print("Average square error reconstruction ={0}".format(obj.level()**2/p))
-    print("Average square error data ={0}".format(np.array([n**2 for n in noise]).sum()/p))
-    print("TODO : also print error between ref and reconstructed output")
+        #Get results
+        print("Objective value ={0}".format(obj.level()**2))
+        print("Matrix Q")
+        print(Q.level().reshape(n,n))
+        print("Matrix Q_ref")
+        print(Qref)
+        print("Vector q")
+        print(q.level())
+        print("Vector q_ref")
+        print(qref)
+        print("Average square error reconstruction ={0}".format(obj.level()**2/p))
+        print("Average square error data ={0}".format(np.array([n**2 for n in noise]).sum()/p))
+        print("TODO : also print error between ref and reconstructed output")
+    
+    
+    # # Sample input data
+    # n = 5
+    # #asym = np.array([[3.7,-0.2,0,1,3],[2,2,1,0,0],[0,0,3,1,2],[1,0,0.4,5,2],[1.2,0,0.4,-1,3]])
+    # asym = 5*np.random.rand(n,n)
+    # Qref = 0.5*(asym + asym.transpose())
+    # #qref = np.array([-2.3,-2,1,2,1])
+    # qref = 3*np.random.rand(n)
+    # #cref = 2
+    # cref= 2*np.random.rand()
+    # sigma = 0.3
+    # p = 5000
+    # wlist = np.random.rand(p,n)
+    # noise = np.random.normal(loc = 0, scale = sigma, size = p)
+    # noiseless_z=0.5 *np.array([ w.dot(Qref).dot(w) for w in wlist]) + wlist.dot(qref) + cref
+    # print(noiseless_z.min())
+    # z = noiseless_z + noise
+    # wlist_square_flattened = np.array([(w.reshape(n,1).dot(w.reshape(1,n))).reshape(n**2) for w in wlist]) 
+    # #for each vector w, we create a matrix n x n (given by w^T * w) and then a vector n**2
+    
+    # #Definition of the LL polytope : [0,1]^n box
+    # A = np.concatenate([np.eye(n),-np.eye(n)])
+    # b = np.concatenate([np.ones(n),np.zeros(n)])
+    # rho = np.sqrt(n)
+    
+    # #Write the .dat file
+    # moment=time.strftime("%Y-%b-%d__%H_%M_%S",time.localtime()) #to have different .dat files for each python run
+    # f = open("./instance"+moment+".dat","w")
+    # f.write("param n := %d;\n"%n)
+    # f.write("param p_max := %d;\n"%p)
+    # f.write("param r_dim := 0;\n")
+    # f.write("\nparam Q_ref :")
+    # for i in range(n):
+    #     f.write(" %d "%(i+1))
+    # f.write(":=\n")
+    # for i in range(n):
+    #     for j in range(n):
+    #         if j==0:
+    #             f.write("   %d "%(i+1))
+    #         f.write("%f "%Qref[i,j])
+    #         if j==(n-1):
+    #             if i==(n-1):
+    #                 f.write(";")
+    #             f.write("\n")
+    
+    # f.write("\nparam q_ref := ")
+    # for i in range(n):
+    #     f.write("%d "%(i+1))
+    #     f.write("%f "%qref[i])
+    # f.write(";\n\nparam c_ref := %f ;\n\n"%cref)
+    # f.write("\nparam w:")
+    # for i in range(n):
+    #     f.write(" %d "%(i+1))
+    # f.write(":=\n")
+    # for i in range(p):
+    #     for j in range(n):
+    #         if j==0:
+    #             f.write("   %d "%(i+1))
+    #         f.write("%f "%wlist[i,j])
+    #         if j==(n-1):
+    #             if i==(p-1):
+    #                 f.write(";")
+    #             f.write("\n")
+    # f.write("\nparam epsilon := ")
+    # for i in range(p):
+    #     f.write("%d "%(i+1))
+    #     f.write("%f "%noise[i])
+    # f.write(";\n")
+    # f.write("\nparam z := ")
+    # for i in range(p):
+    #     f.write("%d "%(i+1))
+    #     f.write("%f "%z[i])
+    # f.write(";")
+    # f.close()
--- Application2_reformulation.py
+++ Application2_reformulation.py
@@ -1,82 +1,82 @@
-#from mosek.fusion import *
+from mosek.fusion import *
 import sys
 import numpy as np 
 import networkx
 from scipy.linalg import sqrtm
 from DimacsReader import *
 
-#Reading graph file
-f = DimacsReader("DIMACS/myciel4.col")
-M = f.M
-n = f.n
-
-name = "myciel4"
-
-Q1= np.load("Application2_data/"+name+"/bigQ1.npy")
-Q2= np.load("Application2_data/"+name+"/bigQ2_fix.npy")
-q1= np.load("Application2_data/"+name+"/q1.npy")
-q2= np.load("Application2_data/"+name+"/q2_fix.npy")     
-Mcheck = np.load("Application2_data/"+name+"/M.npy")
-
-print(np.linalg.norm(M-Mcheck))
-
-# Create a model with n semidefinite variables od dimension d x d
-
-with Model("App2") as model:
+def __main__(name_dimacs,name):
+    #Reading graph file
+    f = DimacsReader("DIMACS/"+name_dimacs)
+    M = f.M
+    n = f.n
     
-    #Upper level var
-    v = model.variable("v", 1, Domain.unbounded())
-    x = model.variable("x", n, Domain.greaterThan(0.0))
+       
+    Q1= np.load("Application2_data/"+name+"/bigQ1.npy")
+    Q2= np.load("Application2_data/"+name+"/bigQ2_fix.npy")
+    q1= np.load("Application2_data/"+name+"/q1.npy")
+    q2= np.load("Application2_data/"+name+"/q2_fix.npy")     
+    Mcheck = np.load("Application2_data/"+name+"/M.npy")
+    
+    print(np.linalg.norm(M-Mcheck))
+    
+    # Create a model with n semidefinite variables od dimension d x d
+    
+    with Model("App2") as model:
         
-    #LL variables
-    lam = model.variable("lambda", Domain.unbounded())
-    alpha = model.variable("alpha", Domain.greaterThan(0.0))
-    beta = model.variable("beta", Domain.unbounded())
-    
-    #Vars for PSD constraint
-    PSDVar = model.variable(Domain.inPSDCone(n+1))
-    PSDVar_main = PSDVar.slice([0,0], [n,n])
-    PSDVar_vec = Var.flatten(PSDVar.slice([0,n], [n,n+1]))
-    PSDVar_offset = PSDVar.slice([n,n], [n+1,n+1])
-    
-    #Objective
-    model.objective( ObjectiveSense.Minimize, v )
-
-    #Simplex constraint for x
-    model.constraint( Expr.sum(x),  Domain.equalsTo(1) )
-    
-    ##t >= 0.5 x^TQ_1x iif t >= 0.5 ||P_1 x ||^2   iif (t,1, P_1x) \in RotatedCone(n+2)
-    ## This constraint is necessary saturated at the optimum, thus we have t = 0.5 x^TQ_1x
-    P1 = sqrtm(Q1)
-    t = model.variable("t", 1, Domain.unbounded())
-    model.constraint(Expr.vstack(t,1, Expr.mul(P1,x)), Domain.inRotatedQCone(n+2))
-    
-    # -v + t +q_1^Tx + lambda + 2 alpha + beta \leq 0 
-    v_and_player1_cost = Expr.add( Expr.mul(-1,v), Expr.add(t,Expr.dot(q1,x)))
-    sum_of_duals = Expr.add(lam,Expr.add(Expr.mul(2,alpha),beta))
-    model.constraint(Expr.add(v_and_player1_cost,sum_of_duals),Domain.lessThan(0.0))
-    
-    #Constraints to define the several parts of the PSD matrix
-    model.constraint(Expr.sub(Expr.add(0.5*Q2, Expr.mul(alpha,np.eye(n))), PSDVar_main),  Domain.equalsTo(0,n,n) )
-    model.constraint( Expr.sub(Expr.add(0.5*q2, Expr.add(Expr.mul(0.5*M.T,x),Expr.mul(lam,0.5*np.ones(n)))), PSDVar_vec),  Domain.equalsTo(0,n) )
-    model.constraint( Expr.sub(Expr.add(beta, alpha), PSDVar_offset),  Domain.equalsTo(0) )
-
-
-    # Solve
-    model.setLogHandler(sys.stdout)            # Add logging
-    model.writeTask("App2.ptf")                # Save problem in readable format
-    model.solve()
-
-    #Get results
-    print("Objective value ={0}".format(v.level()))
-    xres = x.level()
-    tres = t.level()[0]
-    print("Check rotated cone constraint (t = 0.5 x^TQ_1x) : ", abs(tres-0.5*xres.dot(Q1).dot(xres)))
-    print("Check last coefficient constraint :", PSDVar.level()[-1] - (alpha.level()[0]+beta.level()[0]))
-    print("Upper level solution : ",x.level())
-    print(lam.level() + 2 * alpha.level() + beta.level())
-    print(alpha.level() + beta.level())
+        #Upper level var
+        v = model.variable("v", 1, Domain.unbounded())
+        x = model.variable("x", n, Domain.greaterThan(0.0))
+            
+        #LL variables
+        lam = model.variable("lambda", Domain.unbounded())
+        alpha = model.variable("alpha", Domain.greaterThan(0.0))
+        beta = model.variable("beta", Domain.unbounded())
         
-
-   
+        #Vars for PSD constraint
+        PSDVar = model.variable(Domain.inPSDCone(n+1))
+        PSDVar_main = PSDVar.slice([0,0], [n,n])
+        PSDVar_vec = Var.flatten(PSDVar.slice([0,n], [n,n+1]))
+        PSDVar_offset = PSDVar.slice([n,n], [n+1,n+1])
+        
+        #Objective
+        model.objective( ObjectiveSense.Minimize, v )
     
+        #Simplex constraint for x
+        model.constraint( Expr.sum(x),  Domain.equalsTo(1) )
+        
+        ##t >= 0.5 x^TQ_1x iif t >= 0.5 ||P_1 x ||^2   iif (t,1, P_1x) \in RotatedCone(n+2)
+        ## This constraint is necessary saturated at the optimum, thus we have t = 0.5 x^TQ_1x
+        P1 = sqrtm(Q1)
+        t = model.variable("t", 1, Domain.unbounded())
+        model.constraint(Expr.vstack(t,1, Expr.mul(P1,x)), Domain.inRotatedQCone(n+2))
+        
+        # -v + t +q_1^Tx + lambda + 2 alpha + beta \leq 0 
+        v_and_player1_cost = Expr.add( Expr.mul(-1,v), Expr.add(t,Expr.dot(q1,x)))
+        sum_of_duals = Expr.add(lam,Expr.add(Expr.mul(2,alpha),beta))
+        model.constraint(Expr.add(v_and_player1_cost,sum_of_duals),Domain.lessThan(0.0))
+        
+        #Constraints to define the several parts of the PSD matrix
+        model.constraint(Expr.sub(Expr.add(0.5*Q2, Expr.mul(alpha,np.eye(n))), PSDVar_main),  Domain.equalsTo(0,n,n) )
+        model.constraint( Expr.sub(Expr.add(0.5*q2, Expr.add(Expr.mul(0.5*M.T,x),Expr.mul(lam,0.5*np.ones(n)))), PSDVar_vec),  Domain.equalsTo(0,n) )
+        model.constraint( Expr.sub(Expr.add(beta, alpha), PSDVar_offset),  Domain.equalsTo(0) )
+    
+    
+        # Solve
+        model.setLogHandler(sys.stdout)            # Add logging
+        model.writeTask("App2.ptf")                # Save problem in readable format
+        model.solve()
+    
+        #Get results
+        print("Objective value ={0}".format(v.level()))
+        xres = x.level()
+        tres = t.level()[0]
+        print("Check rotated cone constraint (t = 0.5 x^TQ_1x) : ", abs(tres-0.5*xres.dot(Q1).dot(xres)))
+        print("Check last coefficient constraint :", PSDVar.level()[-1] - (alpha.level()[0]+beta.level()[0]))
+        print("Upper level solution : ",x.level())
+        print(lam.level() + 2 * alpha.level() + beta.level())
+        print(alpha.level() + beta.level())
+            
+    
+       
+        
