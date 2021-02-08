6a971fa4fb1c4f607eff2bc01fd24a897f017e09
aoustry@gmail.com <aoustry@gmail.com>
2021-02-08 09:34:56 +0100

    command for App2bis

--- reformulation_command.py
+++ reformulation_command.py
@@ -8,11 +8,11 @@ import os
 
 #Call application 1
 
-for i in range(1,7):
-    name = "PSD_random"+str(i)
-    os.system("python3 Application1_reformulation.py "+name)
-    name = "nonPSD_random"+str(i)
-    os.system("python3 Application1_reformulation.py "+name)
+# for i in range(1,7):
+#     name = "PSD_random"+str(i)
+#     os.system("python3 Application1_reformulation.py "+name)
+#     name = "nonPSD_random"+str(i)
+#     os.system("python3 Application1_reformulation.py "+name)
 
 #Call application 2
 list_graphs = ["myciel4","myciel7","jean","myciel5","myciel6","queen5_5","queen6_6","queen7_7","queen8_8","queen8_12","queen9_9"]
@@ -20,6 +20,6 @@ list_graphs = ["myciel4","myciel7","jean","myciel5","myciel6","queen5_5","queen6
 for graph in list_graphs:
     nameDimacs = graph+".col"
     name = graph+"_det1"
-    os.system("python3 Application2_reformulation.py "+nameDimacs+" "+name)
+    os.system("python3 Application2bis_reformulation.py "+nameDimacs+" "+name)
     name = graph+"_random1"
-    os.system("python3 Application2_reformulation.py "+nameDimacs+" "+name)
+    os.system("python3 Application2bis_reformulation.py "+nameDimacs+" "+name)
