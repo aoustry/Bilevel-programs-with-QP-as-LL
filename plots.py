eba132e341a1a0e9ebf9db96da053b3f88694f2d
aoustry@gmail.com <aoustry@gmail.com>
2021-04-19 17:17:37 +0200

    plots_app

--- plots.py
+++ plots.py
@@ -20,3 +20,17 @@ def plot_convergence_single(name):
     plt.yscale(r'Feasibility error $\epsilon_k$')
     plt.savefig("plots/"+name+".png")
     
+def plot_convergence_app(app_idx,instance_liste):
+    for name in instance_liste:
+        file = open("epsilon/"+name+".txt")
+        array = []
+        for line in file.readlines():
+            array.append(float(line))
+        
+    plt.plot(array)
+    plt.yscale('log')
+    plt.title("Application {0}, convergence of the feasibility error.".format(app_idx))
+    plt.xlabel("Iterations")
+    plt.yscale(r'Feasibility error $\epsilon_k$')
+    plt.savefig("plots/app_{0}.png".format(app_idx))
+    
