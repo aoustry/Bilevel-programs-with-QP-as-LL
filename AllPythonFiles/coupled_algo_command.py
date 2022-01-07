026c0db2a73e52cb7dd2be2770ca75f5b7cb31de
aoustry@gmail.com <aoustry@gmail.com>
2022-01-07 15:23:30 +0100

    coupled algo

--- AllPythonFiles/coupled_algo_command.py
+++ AllPythonFiles/coupled_algo_command.py
@@ -14,8 +14,8 @@ from Application2_coupled_algo import main_app2
 for i in range(1,10):
     name = "PSD_random"+str(i)
     main_app1(name,0.1)
-    # name = "notPSD_random"+str(i)
-    # main_app1(name,0.1)
+    name = "notPSD_random"+str(i)
+    main_app1(name,0.1)
 
 #Call application 2
 list_graphs = ["jean","myciel4","myciel5","myciel6","myciel7","queen5_5","queen6_6","queen7_7","queen8_8","queen8_12","queen9_9"]
