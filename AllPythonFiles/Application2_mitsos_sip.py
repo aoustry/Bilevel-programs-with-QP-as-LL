5030c40d2f3bc3e56a9fdc9d138b84c702e34170
MartinaCerulli <72506619+MartinaCerulli@users.noreply.github.com>
2022-01-07 22:18:43 +0100

    Update Application2_mitsos_sip.py

--- AllPythonFiles/Application2_mitsos_sip.py
+++ AllPythonFiles/Application2_mitsos_sip.py
@@ -42,9 +42,9 @@ def main_app2(name_dimacs,name,r=10,timelimit=18000):
     assert(np.linalg.norm(M-M.T)<1E-6)
     assert(np.linalg.norm(Q1-Q1.T)<1E-6)
     assert(np.linalg.norm(Q2-Q2.T)<1E-6)
-    t0 = time.time()
-       
     
+    print("We are solving instance:", name)
+    t0 = time.time()
     relax = gp.Model("relax problem")
     xvar = relax.addMVar(n,lb=0,ub=1,name='x')
     zvar = relax.addMVar(1,name='z',lb=-GRB.INFINITY,ub=GRB.INFINITY)
