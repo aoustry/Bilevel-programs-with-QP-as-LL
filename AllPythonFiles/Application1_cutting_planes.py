29233fe751503153b43c4735acf4792196353a8c
MartinaCerulli <72506619+MartinaCerulli@users.noreply.github.com>
2022-01-07 16:28:33 +0100

    Update Application1_cutting_planes.py

--- AllPythonFiles/Application1_cutting_planes.py
+++ AllPythonFiles/Application1_cutting_planes.py
@@ -1,10 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Dec  2 10:01:47 2021
-
-@author: aoust
-"""
-
 import gurobipy as gp
 from gurobipy import GRB
 import numpy as np
@@ -13,50 +6,62 @@ from itertools import combinations
 import pandas as pd
 
 
-def save(name,finished, p,value, soltime,itnumber, bigQ, q,c):
+def save(name,finished,p,value,soltime,iteration,bigQ,q,c):
     f = open("../output/Application1/"+name+"/cutting_plane.txt","w+")
-    f.write('Finished before TL = {0}'.format(finished))
+    if finished==True:
+        f.write("Finished before time limit.\n")
+    else:
+        f.write("Time limit reached.\n")
     f.write("Obj value returned by the CP solver: "+str(value)+"\n")
-    f.write("Average LSE = {0}".format(value/p))
+    f.write("Average LSE: {0}\n".format(value/p))
     f.write("SolTime: "+str(soltime)+"\n")
-    f.write("It. number: "+str(itnumber)+"\n")
-    f.write("Q matrix recovered: " +str(bigQ) +"\n")
+    f.write("It. number: "+str(iteration)+"\n")
+    f.write("\nQ matrix recovered: " +str(bigQ) +"\n")
     f.write("q vector recovered: " +str(q) +"\n")
     f.write("Scalar c recovered: " +str(c) +"\n")
     f.close()
 
 def main_app1(name,timelimit=18000):
     #Logs
-    UpperBoundsLogs,LowerBoundsLogs, EpsLogs, MasterTimeLogs, LLTimeLogs = [], [],[],[],[]
+    UpperBoundsLogs, LowerBoundsLogs, EpsLogs, MasterTimeLogs, LLTimeLogs = [], [],[],[],[]
+    
     #Loading data
     wlist = np.load("../Application1_data/"+name+"/w.npy")
     p,n = wlist.shape
     z = np.load("../Application1_data/"+name+"/z.npy")
     wlist_square_flattened = np.array([(w.reshape(n,1).dot(w.reshape(1,n))).reshape(n**2) for w in wlist]) 
+    
     t0 = time.time()
     master = gp.Model("Master problem")
+    #variables
     flattenedQvar = master.addMVar(n**2,lb=-GRB.INFINITY,ub=GRB.INFINITY,name='flattenedQmatrix')
     spread = master.addMVar(p,lb=-GRB.INFINITY,ub=GRB.INFINITY)
     qvar = master.addMVar(n,name='qvector',lb=-GRB.INFINITY,ub=GRB.INFINITY)
     cvar = master.addMVar(1,name='c',lb=-GRB.INFINITY,ub=GRB.INFINITY)
-    master.addConstr(spread== z-0.5*wlist_square_flattened@flattenedQvar - wlist@qvar - np.ones((p,1))@cvar)
+    #constraints
+    master.addConstr(spread == z-0.5*wlist_square_flattened@flattenedQvar - wlist@qvar - np.ones((p,1))@cvar)
     for (i,j) in combinations(range(n),2):
           master.addConstr(flattenedQvar[i*n+j]==flattenedQvar[j*n+i])
+    #objective function
     master.setObjective(spread@spread, GRB.MINIMIZE)
-    running, itnumber = True,0
+    
+    running,iteration = True,0
     while running and (time.time()-t0<timelimit):
+        iteration+=1
         t1 = time.time()
         master.optimize()
         mastertime = time.time() - t1
         flattenedQ,q,c = flattenedQvar.X, qvar.X, cvar.X
         Q = flattenedQ.reshape((n,n))
-        itnumber+=1
+        
         t1 = time.time()
         tl = 10+max(0,timelimit-(t1-t0))
         y,val = solve_subproblem_App1(n,Q,q,c,tl)
         LLtime = time.time() - t1
-        #Log
+        
+        #computing an Upper Bound
         ub = objective_value(n,Q,q,c+max(0,-val),wlist,z)
+        #Log
         UpperBoundsLogs.append(ub)
         LowerBoundsLogs.append(master.objVal)
         EpsLogs.append(val)
@@ -68,7 +73,7 @@ def main_app1(name,timelimit=18000):
             Y = (y.reshape(n,1).dot(y.reshape(1,n))).flatten()
             master.addConstr(0.5*Y@flattenedQvar + y@qvar + cvar >=0)
     soltime = time.time() - t0
-    save(name,not(running), p,master.objVal, soltime, itnumber,flattenedQ, q,c)
+    save(name,not(running),p,master.objVal,soltime,iteration, flattenedQ,q,c)
     df = pd.DataFrame()
     df['UB'],df['LB'],df["Epsilon"],df["MasterTime"],df['LLTime'] = UpperBoundsLogs, LowerBoundsLogs, EpsLogs, MasterTimeLogs, LLTimeLogs
     df.to_csv("../output/Application1/"+name+"/cutting_plane.csv")
